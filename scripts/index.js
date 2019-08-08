3138aa810028d508cb9332c9380fb4debe5bb189
Chris Matian <chris.matian@race.com>
2019-08-07 17:46:23 -0700

    Updated pass tos branch

--- scripts/index.js
+++ scripts/index.js
@@ -25,7 +25,7 @@ let instr = {
         drive: "The barricade is now disengaged. Please pass through and drive safely. Thank you."
     },
     failed: {
-        msg: "The device has determined that you are not sober."
+        msg: "The device has determined that you are not sober.",
         stepOne: "Please pull over the vehicle to the indicated area on the display.",
         stepTwo: "Failure to heed this instruction will result in the EMP device disabling your vehicle permanently.",
         stepThree: "The assigned police units for this device have been notified, and they are on their way to evaluate you further."
@@ -35,4 +35,38 @@ let instr = {
 // 1. Display the page
 // 2. Wait 2 seconds and then scroll down to the bottom of the page
 // 3. After reaching the bottom - wait 5 seconds and then redirect to the next instruction page.
+
+// waitFor generates a promise each time which returns the result after a set time
+const waitFor = (time) => new Promise(res => setTimeout(res, time));
+
+// Async function for looping through an array and executing a callback
+async function asyncForEach(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        await callback(array[i], i, array);
+    }
+}
+
+const init = async () => {
+
+    const nodeList = document.querySelectorAll('.aux-target');
+
+    asyncForEach(nodeList, async (item, i, array) => {
+        // Render the item
+        item.classList.remove('hidden', 'remove-from-dom');
+
+        // Wait 3 seconds
+        await waitFor(10000);
+
+        // If last item don't hide
+        if((i + 1) === array.length) {
+            return;
+        }
+
+        // Hide the element
+        item.classList.add('hidden');
+
+        // Wait 1000 ms until it has faded out before looping again
+        await waitFor(1000);
+    });
+};
 
